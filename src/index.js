bec9f7207bc1528a78017beddf26337a7b156cfa
xiaguangding <v_xiaguangding@weshare.local>
2020-09-27 17:34:54 +0800

    feat: 回退处理

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import _ from 'lodash';
-import './style.css';
-import mayuyu from './img.jpg'
-import Data from './data.xml';
+
 
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  var myIcon = new Image();
-  myIcon.src = mayuyu;
-  element.appendChild(myIcon);
   
-  console.log(Data);
   return element;
 }
 
