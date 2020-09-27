8cf53be2ff6d9bf6c81d3fdd927763389c0b76c3
fuheideMayuyu <xiaguangding@163.com>
2020-09-27 23:47:07 +0800

    feat: webpack 预取和预加载

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
-import _ from 'lodash';
-import printMe from './print.js';
 
+async function getComponent() {
+  const element = document.createElement('div');
+   const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-  element.appendChild(btn);
-
-  return element;
+   return element;
 }
 
-document.body.appendChild(component());
-// if(module.hot){
-//   module.hot.accept('./print.js', function() {
-//     console.log('Accepting the updated printMe module!');
-//     printMe();
-//   })
-// }
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    index: './src/index.js',
+   
   },
+
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+   
   },
+
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
     // hot: true
   },
+
   plugins:[
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({
       title: 'Development'
     }),
-    
   ]
 };
